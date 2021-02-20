86d65c31db2691469c2cd2ff49176323f883fed0
nstppbl <1nstppbl@gmail.com>
2021-02-20 14:13:07 +0500

    init: create function for check brackets balance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2) {
+        return false;
+    }
+    let stack = [];
+
+
+    for (let i = 0; i < str.length; i++) {
+
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] == stack[stack.length - 1]) {
+                stack.pop();
+                continue;
+            }
+
+            if (str[i] == bracketsConfig[j][0]) {
+                stack.push(str[i]);
+            }
+
+        }
+
+    }
+
+    if (stack.length != 0) {
+        return false;
+    }
+
+    return true;
+
 }
+
